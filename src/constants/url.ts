d21142f3b5e27bee2d0f734ea23739608dbac96c
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2024-12-18 19:22:10 +0530

    change server url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,7 +4,11 @@ const hostname: HostType = window.location.hostname as HostType;
 
 export const BASE_URL = "http://localhost:5173";
 export const API_URL =
-  hostname == "localhost" ? "http://127.0.0.1:5500/api/v1" : "";
+  hostname == "localhost"
+    ? "http://127.0.0.1:5500/api/v1"
+    : "http://13.233.121.43:5500/api/v1";
 
 export const FILE_URL =
-  hostname == "localhost" ? "http://127.0.0.1:5500/uploads" : "";
+  hostname == "localhost"
+    ? "http://127.0.0.1:5500/uploads"
+    : "http://13.233.121.43:5500/uploads";
