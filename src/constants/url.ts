a0fb028511ad643a0dd0beb7161aec5b8de3f741
Bikash Kumar Singh <bikashkumarsingh@Bikashs-MacBook-Air.local>
2026-02-16 19:01:32 +0530

    add s to http

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -6,9 +6,9 @@ export const BASE_URL = "http://localhost:5173";
 export const API_URL =
   hostname == "localhost"
     ? "http://127.0.0.1:5500/api/v1"
-    : "http://gunjankamrayoga.com/server/api/v1";
+    : "https://gunjankamrayoga.com/server/api/v1";
 
 export const FILE_URL =
   hostname == "localhost"
     ? "http://127.0.0.1:5500/uploads"
-    : "http://gunjankamrayoga.com/server/uploads";
+    : "https://gunjankamrayoga.com/server/uploads";
